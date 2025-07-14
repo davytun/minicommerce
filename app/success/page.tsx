67e25d72dc9f81456cb5121eca26e652af817c02
Davytun <davidakintunde433@gmail.com>
2025-07-14 18:42:17 +0100

    the home page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,9 +1,18 @@
+import { Suspense } from "react";
 import Success from "@/components/Success/Success";
 
 export default function Page() {
   return (
     <main>
-      <Success />
+      <Suspense
+        fallback={
+          <div className="flex items-center justify-center min-h-screen">
+            <div className="w-12 h-12 border-4 border-gray-900 border-t-transparent rounded-full animate-spin"></div>
+          </div>
+        }
+      >
+        <Success />
+      </Suspense>
     </main>
   );
 }
